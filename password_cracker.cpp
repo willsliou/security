dd600ecaafa9b0b8ff5cf0fe6a8111f3569cc660
Wills Liou <willsliou@protonmail.com>
2021-03-08 17:19:32 -0800

    Create password_cracker.cpp

---
+++ password_cracker.cpp
@@ -0,0 +1,48 @@
+// Top 10 most common passwords retrieved from security breaches 2017
+
+// John the Ripper password cracker - dictionary method 
+
+#include <iostream>
+#include <string>
+using std::string;
+
+// Top 10 common passwords
+const string p1 = "123456";
+const string p2 = "password";
+const string p3 = "12345678";
+const string p4 = "qwerty";
+const string p5 = "12345";
+const string p6 = "12345789";
+const string p7 = "letmein";
+const string p8 = "123567";
+const string p9 = "football";
+const string p10 = "iloveyou";
+
+const char alphabet_lower[26] {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z'
+};
+
+const char alphabet_upper[26] { 
+	'A', 'B', 'C', 'D', 'E', 'F', 'G',
+	'H', 'I', 'J', 'K', 'L', 'M', 'N',
+	'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+	'V', 'W', 'X', 'Y', 'Z'
+};
+
+// https://en.cppreference.com/w/cpp/language/ascii
+const int num_ascii_begin = 32; // 32 is space
+const int num_ascii_end = 126; // 126 is ~
+
+
+
+void cracker() {
+
+}
+
+
+int main() {
+    
+}
